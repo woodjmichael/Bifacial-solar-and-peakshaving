b9f78b36540ef2de57b303e723e2fd440405e76a
Michael Wood <35273554+woodjmichael@users.noreply.github.com>
2024-07-16 12:49:22 +0200

    final work for paper

--- paper/make.sh
+++ paper/make.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
-#cp paper.tex ./tex/paper.tex;
-#cp mendeley.bib ./tex/mendeley.bib;
-#cd tex;
+cd paper
+
 pdflatex -interaction=nonstopmode paper;
 bibtex paper;
 pdflatex -interaction=nonstopmode paper;
 pdflatex -interaction=nonstopmode paper;
-#rm paper.tex
-#rm mendeley.bib
-#mv paper.pdf ../paper.pdf
+
+#mv paper.pdf paper/paper.pdf
 
 rm paper.aux;
 rm paper.bbl;
 rm paper.blg;
-rm paper.log;
+#rm paper.log;
+rm paper.out;
